9de1b3f5d2c5ffad9e90f62bdbddf8567bed8a56
gle-mini <gle-mini@Inception.inception.fr>
2023-12-21 13:47:04 -0500

    Still changing createdb.sql

--- srcs/requirements/mariadb/conf/createdb.sql
+++ srcs/requirements/mariadb/conf/createdb.sql
@@ -4,3 +4,16 @@ GRANT ALL PRIVILEGES ON Base.* TO 'usera'@'%';
 FLUSH PRIVILEGES;
 
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'Gurvan05';
+
+
+-- -- Créer une nouvelle base de données
+-- CREATE DATABASE IF NOT EXISTS wordpress_db;
+
+-- -- Créer un nouvel utilisateur et définir un mot de passe
+-- CREATE USER IF NOT EXISTS 'wp_user'@'%' IDENTIFIED BY 'wp_pass';
+
+-- -- Accorder tous les privilèges à l'utilisateur sur la base de données
+-- GRANT ALL PRIVILEGES ON wordpress_db.* TO 'wp_user'@'%';
+
+-- -- Appliquer immédiatement les privilèges
+-- FLUSH PRIVILEGES;
