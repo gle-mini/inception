c63fe122a9a8860426794e95f20de174b9685d9a
Gurvan Le Minier <gleminier2@gmail.com>
2023-12-23 01:55:57 +0100

    add site, working OK

--- srcs/requirements/bonus/site/conf/run.js
+++ srcs/requirements/bonus/site/conf/run.js
@@ -1,9 +1,43 @@
-const http = require('http')
-const fs = require('fs')
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/html' })
-  fs.createReadStream('index.html').pipe(res)
-})
+  let filePath = '.' + req.url;
+  if (filePath === './') {
+    filePath = './index.html';
+  }
 
-server.listen(process.env.PORT || 1111)
+  const extname = String(path.extname(filePath)).toLowerCase();
+  const mimeTypes = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    // Ajoutez d'autres types MIME si nécessaire
+  };
+
+  const contentType = mimeTypes[extname] || 'application/octet-stream';
+
+  fs.readFile(filePath, function(error, content) {
+    if (error) {
+      if (error.code === 'ENOENT') {
+        // Page non trouvée
+        fs.readFile('./404.html', function(error, content) {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(content, 'utf-8');
+        });
+      } else {
+        // Quelques erreurs serveur
+        res.writeHead(500);
+        res.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
+      }
+    } else {
+      // Envoie le fichier avec le bon type MIME
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+server.listen(process.env.PORT || 1111, () => {
+  console.log(`Server running at PORT ${process.env.PORT || 1111}`);
+});
