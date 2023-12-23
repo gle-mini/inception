dd920c3d269a6e61a6099a0d927e8892a3d4fed1
Gurvan Le Minier <gleminier2@gmail.com>
2023-12-23 01:57:28 +0100

    remove comments

--- srcs/requirements/bonus/site/conf/run.js
+++ srcs/requirements/bonus/site/conf/run.js
@@ -12,7 +12,6 @@ const server = http.createServer((req, res) => {
   const mimeTypes = {
     '.html': 'text/html',
     '.css': 'text/css',
-    // Ajoutez d'autres types MIME si nécessaire
   };
 
   const contentType = mimeTypes[extname] || 'application/octet-stream';
@@ -20,18 +19,15 @@ const server = http.createServer((req, res) => {
   fs.readFile(filePath, function(error, content) {
     if (error) {
       if (error.code === 'ENOENT') {
-        // Page non trouvée
         fs.readFile('./404.html', function(error, content) {
           res.writeHead(200, { 'Content-Type': 'text/html' });
           res.end(content, 'utf-8');
         });
       } else {
-        // Quelques erreurs serveur
         res.writeHead(500);
         res.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
       }
     } else {
-      // Envoie le fichier avec le bon type MIME
       res.writeHead(200, { 'Content-Type': contentType });
       res.end(content, 'utf-8');
     }
